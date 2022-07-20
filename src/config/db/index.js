8e34285c4128ee9c1b7527a67bd9d0b0e3ae2839
vananz123 <88036322+vananz123@users.noreply.github.com>
2022-07-20 21:36:28 +0700

    comit ke

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
-async function connect(){
-    try{
-        await mongoose.connect('mongodb://localhost:27017/f8_dev');
-        console.log("oke")
-    }catch(error){
-        console.log("0 oke")
-    }
-}
-module.exports ={connect}
+// const mongoose = require('mongoose');
 // async function connect(){
 //     try{
-//         const { MongoClient, ServerApiVersion } = require('mongodb');
-//         const uri = "mongodb+srv://vananz:mn112233@cluster0.shsiivx.mongodb.net/?retryWrites=true&w=majority";
-//         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-//         client.connect(err => {
-//             console.log('ko ket noi dc')
-//         client.close();
-//         });
+//         await mongoose.connect('mongodb://localhost:27017/f8_dev');
 //         console.log("oke")
-//     }catch{
+//     }catch(error){
 //         console.log("0 oke")
 //     }
-
 // }
 // module.exports ={connect}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+function connectdb(){
+    try{
+        const uri = "mongodb+srv://vananz:mn112233@cluster0.shsiivx.mongodb.net/?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        client.connect(err => {
+            console.log('ko ket noi dc')
+        client.close();
+        });
+       
+    }catch{
+        console.log("0 oke")
+    }
+
+}
+module.exports ={connectdb}
 
