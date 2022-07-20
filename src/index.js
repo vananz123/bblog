9953e015c0b0c450968e987865f5b574ac3003ce
vananz123 <88036322+vananz123@users.noreply.github.com>
2022-07-20 15:08:34 +0700

    fff

--- src/index.js
+++ src/index.js
@@ -40,9 +40,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://vananz:mn112233@cluster0.shsiivx.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-const collection = client.db("test").collection("devices");
-// perform actions on the collection object
-client.close();
+  console.log('ko ket noi dc')
+  client.close();
 })
 //middeware trả user ra all view cần fix lôi bảo mật password 
 app.use(function(req, res, next) {
