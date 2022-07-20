aab9ab67909d7ef296985aff0a0d93c0d2e4bb47
vananz123 <88036322+vananz123@users.noreply.github.com>
2022-07-20 14:04:48 +0700

    ff

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// async function connect(){
+//     try{
+//         await mongoose.connect('mongodb://localhost:27017/f8_dev');
+//         console.log("oke")
+//     }catch(error){
+//         console.log("0 oke")
+//     }
+// }
+// module.exports ={connect}
+
+const mongoAtlasUri = "mongodb+srv://vananz:<password>@cluster0.shsiivx.mongodb.net/?retryWrites=true&w=majority";
 async function connect(){
-    try{
-        await mongoose.connect('mongodb://localhost:27017/f8_dev');
-        console.log("oke")
-    }catch(error){
-        console.log("0 oke")
+    try {
+        // Connect to the MongoDB cluster
+        mongoose.connect(
+            mongoAtlasUri,
+            { useNewUrlParser: true, useUnifiedTopology: true },
+            () => console.log(" Mongoose blog mindx is connected")
+        );
+    } catch (e) {
+        console.log("could not connect");
     }
 }
 module.exports ={connect}
